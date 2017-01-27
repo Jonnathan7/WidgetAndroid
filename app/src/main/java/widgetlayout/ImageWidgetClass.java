ddc1b0ad05080a605878d89ad7d324fbfbbe9b3b
Jonnathan De La Barra <justindelabarra@gmail.com>
2017-01-27 02:00:41 -0500

    Initial commit by Jonnathan De La Barra

---
+++ app/src/main/java/widgetlayout/ImageWidgetClass.java
@@ -0,0 +1,17 @@
+package widgetlayout;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Jonnathan on 25/01/2017.
+ */
+
+public class ImageWidgetClass extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
